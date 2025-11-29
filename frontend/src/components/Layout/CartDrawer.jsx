08ffc6adf998de0b22b01cce32f279fb873cf923
shohi-11 <shohilanis@gmail.com>
2025-11-29 22:16:55 +0530

    Update the today changes

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { IoMdClose } from 'react-icons/io';
 import CartContents from '../Cart/CartContents';
+import { useNavigate } from 'react-router-dom';
 
 const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
+    const navigate = useNavigate();
+    const handleCheckout = () => {
+        navigate("/checkout");
+    }
     
     return (
     <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform 
@@ -23,7 +28,8 @@ const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
          </div>
          {/*Checkout button fixed at the bottom */}
          <div className='p-4 bg-white sticky bottom-0'>
-            <button className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800
+            <button onClick={handleCheckout}
+            className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800
             transition'>
                 Checkout</button>
             <p className='text-sm tracking-tighter text-gray-500 mt-2 text-center'>
