c3c864eb12b99f62bbeb4eb12d00e471c37da819
shohi-11 <shohilanis@gmail.com>
2025-12-22 20:27:39 +0530

    Update the new file and changes in rabbit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require ("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require ("./Config/db");
+const userRoutes = require ("./routes/userRoutes")
 
 const app = express();
 app.use(express.json());
@@ -11,9 +13,15 @@ dotenv.config();
  
 const PORT = process.env.PORT || 3000;
 
+// connect to MongoDB
+connectDB();
+
 app.get("/", (req, res) => {
     res.send("WELCOME TO RABBIT API!");
 });
+
+// API Routes
+app.use("/api/users", userRoutes);
 
 app. listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
